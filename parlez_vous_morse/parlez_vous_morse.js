ce8848b8fb795d2b9c1b8f7fa63f8e107bb695e8
Meyko_Dev <raphaelle.noel@gmail.com>
2025-07-13 07:48:47 +0200

    latinToMorse effective" latinToMorse effective

--- parlez_vous_morse/parlez_vous_morse.js
+++ parlez_vous_morse/parlez_vous_morse.js
@@ -1,86 +1,4 @@
-// // Function to translate a Latin character to Morse code
-
-// const latinToMorse = {
-// 	'A':'.-',
-// 	'B':'-...',
-// 	'C':'-.-.',
-// 	'D':'-..',
-// 	'E':'.',
-// 	'F':'..-.',
-// 	'G':'--.',
-// 	'H':'....',
-// 	'I':'..',
-// 	'J':'.---',
-// 	'K':'-.-',
-// 	'L':'.-..',
-// 	'M':'--',
-// 	'N':'-.',
-// 	'O':'---',
-// 	'P':'.--.',
-// 	'Q':'--.-',
-// 	'R':'.-.',
-// 	'S':'...',
-// 	'T':'-',
-// 	'U':'..-',
-// 	'V':'...-',
-// 	'W':'.--',
-// 	'X':'-..-',
-// 	'Y':'-.--',
-// 	'Z':'--..'
-// }
-
-// const text =[ "H", "e", "l", "l", "o", ",", " ", "w", "o", "r", "l", "d" ]
-
-// function translatemorsecode(code){
-// 	return latinToMorse[code.toUpperCase()] || ''  ;
-// }
-// console.log(translatemorsecode('C'))
-
-
-// function getLatinCharacterList(text) {
-//     for (let code = 0; code < text.length; i++) {
-//        return latinToMorse[text[code].toUpperCase()] || '';
-//    } 
-// }
-// console.log(getLatinCharacterList(text));
-
-
-
-// // Function to translate a Morse code to a Latin character
-// const morseToLatin = {
-//   '-': "T",
-//   '--': "M",
-//   '---': "O",
-//   '--.': "G",
-//   '--.-': "Q",
-//   '--..': "Z",
-//   '-.': "N",
-//   '-.-': "K",
-//   '-.--': "Y",
-//   '-.-.': "C",
-//   '-..': "D",
-//   '-..-': "X",
-//   '-...': "B",
-//   '.': "E",
-//   '.-': "A",
-//   '.--': "W",
-//   '.---': "J",
-//   '.--.': "P",
-//   '.-.': "R",
-//   '.-..': "L",
-//   '..': "I",
-//   '..-': "U",
-//   '..-.': "F",
-//   '...': "S",
-//   '...-': "V",
-//   '....': "H"
-// }
-
-
-// //function getmorseCharacterList(string,) { 
-//    //return morseToLatin[string] || '';
-// //}
-// //console.log(getmorseCharacterList('..-.'))
+/* global document */
 
 const latinToMorse = {
 	'A':'.-',
@@ -114,10 +32,11 @@ const latinToMorse = {
 function getLatinCharacterList(text){
     return Array.from(text).map(char => latinToMorse[char.toUpperCase()] || '')
     .join(' ');
-}console.log(getLatinCharacterList("Hello, world"));
+}
+//console.log(getLatinCharacterList("Hello, world"));
 
 
-//////
+///
 
 function translateLatinCharacter(character){
     return latinToMorse[character.toUpperCase()] || '';
@@ -131,10 +50,18 @@ console.log(translateLatinCharacter('A'));
 //     return prompt.split('').map(char => latinToMorse[char.toUpperCase()] || '').join(' ');
 // }
 // console.log(encode(prompt=''))
-const translateButton = document.getElementById("translateButton")
+//const translateButton = document.getElementById("translateButton")
+
+translateButton = document.getElementById("translateButton");
 
 translateButton.addEventListener("click", () => { 
+    console.log("Button clicked");
+    
     const inputText = document.getElementById("inputText").value;
+    console.log("recup text:", inputText);
+    
     const morseCode = getLatinCharacterList(inputText);
+    console.log("function result", morseCode);
+    
     document.getElementById("output").innerText = morseCode;
 });
