abde86aa5747bbdfa514f2dac1faf068b3cdb63b
Meyko_Dev <raphaelle.noel@gmail.com>
2025-07-18 14:48:55 +0200

    palindrome 1

---
+++ palindromes/palindrome.js
@@ -0,0 +1,12 @@
+function isValidDate(dateString){
+       // Expression régulière pour le format dd/mm/aaaa
+    const dateRegex = /^(\d{2})\/(\d{2})\/(\d{4})$/;
+    const match = dateString.match(dateRegex);
+    if (!match) {
+        return false;
+}
+
+function maxDaysInMonth(){
+
+
+}
